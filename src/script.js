20cb7f65c29840b5a913e8aab5792b9bd70e660c
Ameer <ameerapex496@gmail.com>
2023-12-01 00:34:44 +0100

    selected our elements

--- src/script.js
+++ src/script.js
@@ -1,1 +1,10 @@
-console.log("hallo world");
+// selecting our elements
+var hourEl = document.getElementsByClassName("hr-Bar")[0];
+var minuteEl = document.getElementsByClassName("min-Bar")[0];
+var secondEl = document.getElementsByClassName("sec-Bar")[0];
+var timeEl = document.getElementsByClassName("time-El")[0];
+var dateEl = document.getElementsByClassName("date-El")[0];
+var dayEl = document.getElementsByClassName("day-El")[0];
+var toggleBtn = document.getElementsByClassName("dark-Light-Btn")[0];
+// here is our functions
+// here is our eventlisnters
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,14 @@
-console.log("hallo world");
+// selecting our elements
+const hourEl = document.getElementsByClassName("hr-Bar")[0] as HTMLElement;
+const minuteEl = document.getElementsByClassName("min-Bar")[0] as HTMLElement;
+const secondEl = document.getElementsByClassName("sec-Bar")[0] as HTMLElement;
+const timeEl = document.getElementsByClassName("time-El")[0] as HTMLElement;
+const dateEl = document.getElementsByClassName("date-El")[0] as HTMLElement;
+const dayEl = document.getElementsByClassName("day-El")[0] as HTMLElement;
+const toggleBtn = document.getElementsByClassName(
+  "dark-Light-Btn"
+)[0] as HTMLElement;
+
+// here is our functions
+
+// here is our eventlisnters
